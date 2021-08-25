323543a47c069cc96c2b6e00ba0961a0253a7441
Sola Otudeko <sola@miles.local>
2021-08-25 11:08:31 +0100

    update

---
+++ demo/src/main/java/org/demo/App.java
@@ -0,0 +1,17 @@
+package org.demo;
+
+import org.cyclonedx.CycloneDxSchema;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        CycloneDxSchema c = new CycloneDxSchema();
+        c.getXmlSchema(arg0)
+    }
+}
